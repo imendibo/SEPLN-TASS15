41051af301e4e18b3730c9b46ab28dfed86bad7a
jeronics <jeronicarandell@gmail.com>
2015-06-12 10:58:20 +0200

    Change OvsA estimator to svm.SVC with more promising results

--- src/classifiers.py
+++ src/classifiers.py
@@ -1,35 +1,25 @@
 __author__ = 'Iosu'
-
-
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm
 from sklearn.multiclass import OneVsRestClassifier as OvsA
-
 
 def classifier_randomForest(features, labels):
     print "Training the random forest..."
 
     # Initialize a Random Forest classifier with 100 trees
-    forest = RandomForestClassifier(n_estimators = 100)
+    forest = RandomForestClassifier(n_estimators=50)
 
     # Fit the forest to the training set, using the bag of words as
     # features and the sentiment labels as the response variable
-
     forest = forest.fit(features, labels)
-
     return forest
 
-
 def classifier_svm(features, labels):
-
-    clf = svm.LinearSVC()
-    clf.fit(features, labels)
-
-    return clf
-
+    clf_svm = svm.LinearSVC()
+    clf_svm.fit(features, labels)
+    return clf_svm
 
 def onevsall(tweets_features, train_labels):
-    clf = OvsA.OneVsRestClassifier(svm.LinearSVC(random_state=0))
-    OvsA.fit(tweets_features, train_labels)
-
-    return clf
+    clf_ova = OvsA(svm.SVC(random_state=0))
+    clf_ova.fit(tweets_features, train_labels)
+    return clf_ova
--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 __author__ = 'Iosu'
 
-
 import xmlreader as xml
 import utils as ut
 import numpy as np
@@ -8,42 +7,42 @@ import BagOfWords as bow
 import classifiers as clf
 
 
-def test(forest, svm, onevsall, vectorizer, test_tweets, test_labels):
-
+def test(forest, svm, oneVSall, vectorizer, test_tweets, test_labels):
     pred = vectorizer.transform(test_tweets)
     pred = pred.toarray()
     print pred
 
-    resultOvsA = oneVSall.predict(pred)
-    resultRF = forest.predict(pred)
     resultSVM = svm.predict(pred)
+    resultRF = forest.predict(pred)
+    resultOvsA = oneVSall.predict(pred)
 
     count = 0
     for idx, result in enumerate(resultSVM):
         if result == test_labels[idx]:
-            count = count + 1
+            count += 1
 
     print count
     print len(resultSVM)
-    print 'accuracy svm: ' + str((count*100)/len(resultSVM))
+    print 'accuracy svm: ' + str((count * 100) / len(resultSVM))
+
     count = 0
     for idx, result in enumerate(resultRF):
         if result == test_labels[idx]:
-            count = count + 1
+            count += 1
 
     print count
     print len(resultRF)
-    print 'accuracy rf: ' + str((count*100)/len(resultRF))
-
+    print 'accuracy rf: ' + str((count * 100) / len(resultRF))
 
     count = 0
     for idx, result in enumerate(resultOvsA):
         if result == test_labels[idx]:
-            count = count + 1
+            count += 1
 
     print count
     print len(resultOvsA)
-    print 'accuracy one vs all: ' + str((count*100)/len(resultOvsA))
+    print 'accuracy one vs all: ' + str((count * 100) / len(resultOvsA))
+
 
 if __name__ == "__main__":
 
@@ -64,37 +63,33 @@ if __name__ == "__main__":
     # total_labels = []
     #
     # for tweet in tokenized_tweets:
-    #     total_tweets.append(tweet['clean'])
-    #     total_labels.append(tweet['class'])
+    # total_tweets.append(tweet['clean'])
+    # total_labels.append(tweet['class'])
 
     for tweet in tokenized_tweets:
-        if (count <= len(tweets) / 5):
+        if count <= len(tweets) / 5:
             test_tweets.append(tweet['clean'])
             test_labels.append(tweet['class'])
         else:
             train_tweets.append(tweet['clean'])
             train_labels.append(tweet['class'])
-
-        count = count + 1
+        count += 1
 
     print len(test_tweets)
     print len(train_tweets)
 
     # for t in train_tweets:
-    #     print t.encode('ascii', 'replace')
-        # clean_tweets.append(tweet['clean'])
-        # labels.append(tweet['class'])
+    # print t.encode('ascii', 'replace')
+    # clean_tweets.append(tweet['clean'])
+    # labels.append(tweet['class'])
 
     train_tweets = np.hstack(train_tweets)
-
     dictionary, tweets_features, vectorizer = bow.bow(train_tweets)
 
     # print dictionary
 
     forest = clf.classifier_randomForest(tweets_features, train_labels)
     svm = clf.classifier_svm(tweets_features, train_labels)
-
     oneVSall = clf.onevsall(tweets_features, train_labels)
-
 
     test(forest, svm, oneVSall, vectorizer, test_tweets, test_labels)
