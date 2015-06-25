964a6443ad5a834d4184dda5c657cfdd05dd47a8
iosu <iosu.mendizabal@gmail.com>
2015-06-25 19:05:22 +0200

    pruebatelegram

--- src/megaloclassificatron.py
+++ src/megaloclassificatron.py
@@ -95,6 +95,7 @@ if __name__ == "__main__":
     Now we have a train_results and test_results. Lets train and test a super classifier
     '''
 
+
     super_clf = clf.classifier_svm(train_results, test_labels)
 
     clf.evaluateResults(super_clf, test_results, validation_labels, estimator_name='Supper Classifier')
